51ea25bbc459e935683067e9edd97d40f22717e7
MarchukOlexsandr <139151197+MarchukOlexsandr@users.noreply.github.com>
2024-01-20 11:25:40 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Baner from "./components/baner";
+import Header from "./components/header";
+import Catalog from "./pages/catalog";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Baner />
+      <Catalog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/baner.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// // import baner from "../assets/baner.jpeg";
+// import images from "../utils/images.js";
+// const Baner = () => {
+//   console.log(images);
+//   const selectedImage = images[0];
+//   return (
+//     <div>
+//       <img src={selectedImage} alt="" />
+//     </div>
+//   );
+// };
