ee904e49f0e9764fd6f824f363dc0f5c1a0cc590
MarchukOlexsandr <139151197+MarchukOlexsandr@users.noreply.github.com>
2024-01-22 18:07:54 +0200

    up

--- src/components/baner.jsx
+++ src/components/baner.jsx
@@ -4,6 +4,7 @@
 // const Baner = () => {
 //   console.log(images);
 //   const selectedImage = images[0];
+//   const selectedImage = images[0];
 //   return (
 //     <div>
 //       <img src={selectedImage} alt="" />
